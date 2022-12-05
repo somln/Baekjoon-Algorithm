1523dd3e72e85df456a7a788b841266718457a5d
somln <92261242+somln@users.noreply.github.com>
2022-12-05 14:07:55 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 380 ms, Memory: 18644 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+		in.close();
+		for(int i=1; i<=n; i++) {
+			for(int k=0; k<n-i; k++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<i; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+   }
+}
