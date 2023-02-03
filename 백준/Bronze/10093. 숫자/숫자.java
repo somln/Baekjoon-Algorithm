a3ed0da0c5cfa33169f618d07e2c43a6991f8b37
SoMin Park <92261242+somln@users.noreply.github.com>
2023-02-03 15:14:45 +0900

    [Bronze II] Title: 숫자, Time: 316 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		
+		int a = in.nextInt();
+		int b = in.nextInt();
+		
+		if(a>b) {
+			int tmp=a;
+			a=b;
+			b=tmp;
+		}
+		
+		if(a==b) System.out.println("0");
+		else {
+			System.out.println(b-a-1);
+		}
+		
+		for(int i=a+1; i<b; i++) {
+			System.out.print(i + " ");
+		}
+		in.close();	
+	}
+}
+
+
