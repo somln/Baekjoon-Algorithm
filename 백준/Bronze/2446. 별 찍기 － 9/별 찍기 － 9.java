60e2230fd05ba2589d45dcfe8ba59d4757702182
SoMin Park <92261242+somln@users.noreply.github.com>
2023-02-03 19:31:39 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 516 ms, Memory: 23332 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+
+		for(int i=N; 0<i; i--) {
+			for(int j=0; j<N-i; j++ ) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<2*i-1; k++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+		
+		
+
+		for(int i=1; i<N; i++) {
+			for(int j=0; j<N-1-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<(2*i)+1; k++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+		in.close();	
+	}
+}
