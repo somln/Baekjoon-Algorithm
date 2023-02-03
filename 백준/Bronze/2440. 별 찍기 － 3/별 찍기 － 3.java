6fafbb90a5c31b59863a1038648fa36989b8d220
SoMin Park <92261242+somln@users.noreply.github.com>
2023-02-03 16:47:10 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 300 ms, Memory: 18028 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		
+		for(int i=N; i>0; i--) {
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		in.close();	
+	}
+
+}
+
