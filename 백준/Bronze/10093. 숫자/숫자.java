99f458a8c87aba07b4f66c52baf7159c4226477a
SoMin Park <92261242+somln@users.noreply.github.com>
2023-02-03 15:18:17 +0900

    [Bronze II] Title: 숫자, Time: 260 ms, Memory: 18160 KB -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.java
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -20,11 +20,9 @@ public class Main {
 			System.out.println(b-a-1);
 		}
 		
-		for(int i=a+1; i<b; i++) {
+		for(long i=a+1; i<b; i++) {
 			System.out.print(i + " ");
 		}
 		in.close();	
 	}
 }
-
-
