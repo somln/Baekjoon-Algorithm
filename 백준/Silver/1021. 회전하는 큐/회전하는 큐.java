28a2026bef04b7950170f919981b776eb8c3e4c1
SoMin Park <92261242+somln@users.noreply.github.com>
2023-08-04 21:30:57 +0900

    [Silver III] Title: 회전하는 큐, Time: 208 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -1,38 +1,41 @@
-
-
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
- 
+
+
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        int N, M;
-        N = sc.nextInt();
-        M = sc.nextInt();
-        for (int i = 1; i <= N; i++) {
-            list.add(i);
-        }
-        int ans = 0;
-        int num = 0;
-        for (int i = 0; i < M; i++) {
-            num = sc.nextInt();
-            while (num != list.get(0)) {
-                if (list.indexOf(num) <= list.size() / 2) {
-                    int first = list.getFirst();
-                    list.removeFirst();
-                    list.addLast(first);
-                } else {
-                    int last = list.getLast();
-                    list.removeLast();
-                    list.addFirst(last);
-                }
-                ans++;
-            }
-            list.removeFirst();
-        }
-        System.out.println(ans);
-    }
- 
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+        Scanner in = new Scanner(System.in);
+		
+		LinkedList<Integer> deque = new LinkedList<Integer>();
+		
+		int count = 0;	
+		
+		int N = in.nextInt();	
+		int M = in.nextInt();	
+		
+		for(int i = 1; i <= N; i++) {
+			deque.offer(i);
+		}
+		
+		 for (int i = 0; i < M; i++) {
+			int key = in.nextInt();
+			int index = deque.indexOf(key);
+			
+			if(index>deque.size()/2) {
+				while(deque.getFirst()!=key) {
+					deque.offerFirst(deque.pollLast());
+					count++;
+				}}			
+			else
+				while(deque.getFirst()!=key) {
+					deque.offerLast(deque.pollFirst());
+					count++;
+			}
+			deque.pollFirst();
+		 }
+		System.out.println(count);
+	}
 }
- 
