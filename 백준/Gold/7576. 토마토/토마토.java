21965472b45144d5b0da90ddbb26da6f3e69c9aa
SoMin Park <92261242+somln@users.noreply.github.com>
2024-03-31 16:25:10 +0900

    [Gold V] Title: 토마토, Time: 1704 ms, Memory: 344880 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,20 +1,21 @@
-import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.Scanner;
 
-
+import java.util.*;
+import java.io.*;
 
 public class Main {
+	
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int m, n;
     static int[][] board;
     static int[][] check;
-
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
+    
+	
+	public static void main(String args[]) throws IOException {
+		
+		Scanner in = new Scanner(System.in);
+		
+		Queue<Pair> queue = new ArrayDeque<>();
 
         m = in.nextInt();
         n = in.nextInt();
@@ -22,59 +23,62 @@ public class Main {
         board = new int[n][m];
         check = new int[n][m];
 
-        Queue<Pair> queue = new ArrayDeque<>();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int x = in.nextInt();
-                board[i][j] = x;
-                if (x == 1) {
+        for(int i=0; i<n; i++) {
+        	for(int j=0; j<m; j++) {
+        		board[i][j] = in.nextInt();
+        		
+                if (board[i][j] == 1) {
                     queue.add(new Pair(i, j));
-                    check[i][j] = 0;
-                }
-            }
+        		}
+        	}
         }
-
-        while (!queue.isEmpty()) {
-            Pair p = queue.poll();
-
-            for (int k = 0; k < 4; k++) {
-                int nx = p.x + dx[k];
-                int ny = p.y + dy[k];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0 && check[nx][ny] == 0) {
-                    check[nx][ny] = check[p.x][p.y] + 1;
-                    queue.add(new Pair(nx, ny));
-                }
-            }
+        
+        while(!queue.isEmpty()) {
+ 
+        	Pair p = queue.poll();
+        	
+        	for(int k=0; k<4; k++) {
+        		
+        		int nx = dx[k] + p.x;
+        		int ny = dy[k] + p.y;
+        		
+        		if(nx>=0 && ny>=0 && nx<n && ny<m && board[nx][ny]==0 && check[nx][ny]==0) {
+        			queue.add(new Pair(nx, ny));
+        			check[nx][ny]=check[p.x][p.y]+1;
+        		}
+        	}
         }
-
-        int max = Arrays.stream(check)
-                .flatMapToInt(Arrays::stream)
-                .max()
-                .orElse(-1);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 0 && check[i][j] == 0) {
-                    System.out.println(-1);
-                    return;
-                }
-            }
+        
+        for(int i = 0; i<n; i++) {
+        	for(int j = 0; j< m; j++) {
+        		if(board[i][j]==0 && check[i][j]==0) {
+        			System.out.println(-1);
+        	 	    return;
+        		}
+        	}
         }
-
-        System.out.println(max);
-    }
-
-    static class Pair {
-        int x;
-        int y;
-
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
+        
+        int max = -1;
+        
+        for(int i = 0; i<n; i++) {
+        	for(int j = 0; j< m; j++) {
+        		if(check[i][j]>max){
+        			max = check[i][j];
+        		}
+        	}
         }
-    }
+        System.out.println(max);   
+	}
+	
+	static class Pair{
+		
+		int x;
+		int y;
+		
+		Pair(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
 }
-
-
