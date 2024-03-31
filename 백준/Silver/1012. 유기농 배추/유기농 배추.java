5029e8438de683e9b4077c03dd9a81fa408654cc
SoMin Park <92261242+somln@users.noreply.github.com>
2024-04-01 02:25:21 +0900

    [Silver II] Title: 유기농 배추, Time: 168 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,85 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+	
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
+	static int[][] board;
+	static int[][] visited;
+	static int n,m;
+	
+	public static void main(String ars[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		while(t-->0) {
+			
+			int cnt = 0;
+			
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			
+			board = new int[n][m];
+			visited = new int[n][m];
+			
+			for(int i = 0; i<k; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				
+				board[x][y] = 1;
+			}
+			
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<m; j++) {
+					if((board[i][j]==1)&&(visited[i][j]==0)) {
+						bfs(i, j);
+						cnt++;
+					}
+				}
+			}
+			System.out.println(cnt);
+		}	
+	}
+	
+	public static void bfs(int i, int j) {
+		
+		Queue<Pair> list = new LinkedList<>();
 
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+		visited[i][j]=1;
+		list.add(new Pair(i, j));
+		
+		while(!list.isEmpty()) {
+			Pair p = list.remove();
+			
+			for(int k =0; k<4; k++) {
+				int nx =  p.x + dx[k];
+				int ny = p.y+ dy[k];
+				
+				if(nx<0 ||  ny<0 || nx>=n || ny>=m) continue;
+				
+				if(board[nx][ny]==1 && visited[nx][ny]==0) {
+					visited[nx][ny] = 1; 
+					list.add(new Pair(nx, ny));
+				}
+			}
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int t = Integer.parseInt(br.readLine());
-
-        for (int testCase = 0; testCase < t; testCase++) {
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            Queue<Pair> qu = new LinkedList<>();
-
-            int[][] board = new int[n][m];
-            int[][] visited = new int[n][m];
-            int count = 0;
-
-            for (int i = 0; i < k; i++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-
-                board[x][y] = 1;
-            }
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-
-                    if (board[i][j] == 0 || visited[i][j] == 1) {
-                        continue;
-                    }
-
-                    count++;
-                    visited[i][j] = 1;
-                    qu.offer(new Pair(i, j));
-
-                    while (!qu.isEmpty()) {
-                        Pair p = qu.poll();
-
-                        for (int f = 0; f < 4; f++) {
-                            int nx = p.x + dx[f];
-                            int ny = p.y + dy[f];
-
-                            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                                continue;
-                            }
-
-                            if (board[nx][ny] == 0 || visited[nx][ny] == 1) {
-                                continue;
-                            }
-                            qu.offer(new Pair(nx, ny));
-                            visited[nx][ny] = 1;
-                        }
-                    }
-                }
-            }
-
-            System.out.println(count);
-        }
-    }
-
-    static class Pair {
-        int x;
-        int y;
-
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	static class Pair{
+		int x;
+		int y;
+		
+		Pair(int x, int y){
+			this.x=x;
+			this.y=y;
+		}
+	}
 }
-
