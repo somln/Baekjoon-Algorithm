998202221afcefb51537b35047f36d56619f3607
SoMin Park <92261242+somln@users.noreply.github.com>
2024-06-15 16:33:39 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.43 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i=0; i<n; i++){
+            String str = Integer.toBinaryString(arr1[i] | arr2[i]);
+            str = String.format("%" + n + "s", str);
+            str = str.replace('1', '#').replace('0', ' ');
+            answer[i] = str;
+        }
+        
+        return answer;
+    }
+}
