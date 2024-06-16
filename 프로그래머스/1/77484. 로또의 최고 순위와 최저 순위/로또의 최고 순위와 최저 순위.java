c12ebe5a52c4389f23413d3980e7e7d8c36f7470
SoMin Park <92261242+somln@users.noreply.github.com>
2024-06-16 15:05:18 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         
-        int answer[] = new int[2];
-        int rank[] = {6,6,5,4,3,2,1};
         int zero = 0, count = 0;
         
         for(int i : lottos){
@@ -15,9 +13,7 @@ class Solution {
             }
         }
         
-        answer[0] = rank[count+zero];
-        answer[1] = rank[count];
-        
+        int answer[] = {Math.min(6, 7-(count+zero)), Math.min(6, 7-(count))};
         return answer;
     }
 }
