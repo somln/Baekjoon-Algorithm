c8b9a502b9743f4c7820e4b01a9518c25dbf55cf
SoMin Park <92261242+somln@users.noreply.github.com>
2024-10-03 11:34:25 +0900

    [Silver III] Title: N과 M (3), Time: 380 ms, Memory: 67844 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int[] arr;
+	static int n;
+	static int m;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		arr = new int[m];
+		func(0);
+		
+		System.out.print(sb.toString());
+	}
+
+	public static void func(int k) {
+		if (k == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+				arr[k] = i;
+				func(k + 1);
+			}
+	
+	}
+}
+
