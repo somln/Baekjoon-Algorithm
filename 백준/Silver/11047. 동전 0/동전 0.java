0604901b74377aefa9e741158714a779ca8daae5
SoMin Park <92261242+somln@users.noreply.github.com>
2025-01-20 18:12:39 +0900

    [Silver IV] Title: 동전 0, Time: 172 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] a = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		int answer =0;
+		for(int i=n-1; i>=0; i--) {
+			answer += k/a[i];
+			k%=a[i];
+		}
+	
+		System.out.println(answer);
+		
+	}
+
+}
