aa0af7a33faa6fd1aff059e12cf36322f2775bda
SoMin Park <92261242+somln@users.noreply.github.com>
2025-01-27 09:36:26 +0900

    [Silver I] Title: 쿼드트리, Time: 112 ms, Memory: 15396 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,52 +1,56 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int[][] arr;
-    static StringBuilder sb = new StringBuilder();
+	static int arr[][];
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		arr = new int[n][n];
+		
+		for (int i = 0; i < n; i++) {
+		    String s = br.readLine(); 
+		    for (int j = 0; j < n; j++) {
+		        arr[i][j] = s.charAt(j) - '0'; 
+		    }
+		}
+		
+		func(0,0,n);
+		System.out.println(sb);
 
-    public static void main(String args[]) {
-
-        Scanner in = new Scanner(System.in);
-
-        int n = in.nextInt();
-        in.nextLine(); // 개행 문자 제거
-        arr = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            String s = in.nextLine();
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
-            }
-        }
-
-        func(0, 0, n);
-        System.out.println(sb.toString());
-    }
-
-    public static void func(int r, int c, int n) {
-        if (check(r, c, n)) {
-            sb.append(arr[r][c]);
-        } else {
-            sb.append('(');
-            int size = n / 2;
-            func(r, c, size);
-            func(r, c + size, size);
-            func(r + size, c, size);
-            func(r + size, c + size, size);
-            sb.append(')');
-        }
-    }
-
-    public static boolean check(int r, int c, int n) {
-        int value = arr[r][c];
-        for (int i = r; i < r + n; i++) {
-            for (int j = c; j < c + n; j++) {
-                if (arr[i][j] != value) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+		
+	}
+	
+	public static void func(int r, int c, int size) {
+		
+		boolean check = true;
+		
+		for(int i=r; i<r+size; i++) {
+			for(int j=c; j<c+size; j++) {
+				if(arr[r][c]!=arr[i][j]) {
+					check= false;
+				}
+			}
+		}
+		
+		if(check) {
+			sb.append(arr[r][c]);
+			return;
+		}else {
+			sb.append('(');
+			int n = size/2;
+			for(int i=0; i<2; i++) {
+				for(int j=0; j<2; j++) {
+					func(r+n*i, c+n*j, n);
+				}
+			}
+		}
+		sb.append(')');
+	}
 }
