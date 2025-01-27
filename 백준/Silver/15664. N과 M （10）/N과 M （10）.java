39a2793b8d410559be83bb3a2eb2d7ffa94576b6
SoMin Park <92261242+somln@users.noreply.github.com>
2025-01-27 17:43:36 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int[] arr;
+	static int n;
+	static int m;
+	static int[] N;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		N = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			N[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(N);
+		arr = new int[m];
+		
+		func(0, 0);
+		System.out.println(sb);
+	}
+	public static void func(int start, int k) {
+		
+		if(k==m) {
+			for(int i=0; i<m; i++) {
+				sb.append(arr[i]).append(' ');
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int tmp = 0;
+		for(int i=start; i<n; i++) {
+			if(N[i]!=tmp) { 
+				arr[k] = N[i];
+				tmp = N[i]; 
+				func(i+1, k+1);
+			}
+			
+		}
+		
+	}
+	
+
+}
